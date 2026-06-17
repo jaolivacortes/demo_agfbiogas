a6adaa0c73f25e8cf9471f676d8246434c03bc69
jaolivac <jaolivac01@suarezdefigueroa.es>
2026-06-17 13:22:15 +0200

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideTranslateService({
       loader: provideTranslateHttpLoader({
-        prefix: '/i18n/',
+        prefix: './i18n/',
         suffix: '.json'
       }),
       fallbackLang: 'es',
